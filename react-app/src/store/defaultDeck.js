57b39707a5d5ce30e6e82d31e3aa7ccc43be8793
Alex Hiller <asonnyhiller@gmail.com>
2023-02-03 00:29:20 -0500

    default deck store + reducer implemented too

--- react-app/src/store/defaultDeck.js
+++ react-app/src/store/defaultDeck.js
@@ -1,5 +1,28 @@
-const defaultDeckReducer = (state = {}, action) => {
+const LOAD_DEFAULT_DECK =  "/decks/getalldecks"
 
+const loadDeck = (payload) => ({
+	type: LOAD_DEFAULT_DECK,
+	payload,
+});
+
+export const getDefaultDeckThunk = (id) => async (dispatch) => {
+	const res = await fetch(`/api/users/decks`);
+
+	if (res.ok) {
+		const payload = await res.json();
+		dispatch(loadDeck(payload));
+		return payload;
+	}
+};
+
+const defaultDeckReducer = (state = {}, action) => {
+    let newState = { ...state };
+	switch (action.type) {
+		case LOAD_DECK:
+			return { ...newState, ...action.payload };
+		default:
+			return state;
+	}
 };
 
 export default defaultDeckReducer;

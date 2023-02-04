7fcf77d01213e62331cb342cb8c9bf168fcee3b4
Alex Hiller <asonnyhiller@gmail.com>
2023-02-04 15:03:56 -0500

    ok reducers done for now

--- react-app/src/store/cards.js
+++ react-app/src/store/cards.js
@@ -1,5 +1,45 @@
-const cardReducer = (state = {}, action) => {
+const LOAD_CARDS = "cards/getallcards"
 
+const loadCards = (payload) => ({
+	type: LOAD_CARDS,
+	payload,
+});
+
+export const getAllCardsThunk = (id) => async (dispatch) => {
+	const res = await fetch(`/api/cards`);
+
+	if (res.ok) {
+		const payload = await res.json();
+		dispatch(loadCards(payload));
+		return payload;
+	}
+};
+
+export const getSingleCard = (id) => async (dispatch) => {
+	const res = await fetch(`/api/cards/${id}`);
+
+	if (res.ok) {
+		const payload = await res.json();
+		dispatch(loadCards(payload));
+		return payload;
+	} else if (res.status < 500) {
+		const data = await res.json();
+		if (data.errors) {
+			return data.errors;
+		}
+	} else {
+		return ["An error occurred. Please try again."];
+	}
+};
+
+const cardReducer = (state = {}, action) => {
+    let newState = { ...state };
+	switch (action.type) {
+		case LOAD_CARDS:
+			return { ...newState, ...action.payload };
+		default:
+			return state;
+	}
 };
 
 export default cardReducer;
--- react-app/src/store/games.js
+++ react-app/src/store/games.js
@@ -1,5 +1,109 @@
-const gameReducer = (state = {}, action) => {
+const LOAD_GAMES = "/games/getallgames";
+const DELETE_GAME = "/products/deletegame";
 
+const loadGames = (payload) => ({
+	type: LOAD_GAMES,
+	payload,
+});
+
+const deleteGame = (payload) => {
+	return {
+		type: DELETE_GAME,
+		payload,
+	};
+};
+
+// GET all games
+export const getAllGamesThunk = () => async (dispatch) => {
+	const response = await fetch(`/api/games`);
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(loadReviews(data));
+		return data;
+	}
+};
+
+//GET specific game by gameId
+export const getGameById = (id) => async (dispatch) => {
+	const response = await fetch(`/api/games/${id}`);
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(loadGames(data));
+		return data;
+	}
+};
+
+// CREATE A GAME
+export const createGameThunk = (data) => async (dispatch) => {
+	const newGame = JSON.stringify(data);
+
+	const response = await fetch(`/api/games`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: newGame,
+	});
+
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(loadGames(data));
+		return null;
+	} else if (response.status < 500) {
+		const data = await response.json();
+		if (data.errors) {
+			return data.errors;
+		}
+	} else {
+		return ["An error occurred. Please try again."];
+	}
+};
+
+// EDIT A GAME
+export const editGameThunk = (game) => async (dispatch) => {
+	const editedGame = JSON.stringify(game);
+	const response = await fetch(`/api/games/${game.id}`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: editedGame,
+	});
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(loadGames(data));
+		return null;
+	} else if (response.status < 500) {
+		const data = await response.json();
+		if (data.errors) {
+			return data.errors;
+		}
+	} else {
+		return ["An error occurred. Please try again."];
+	}
+};
+
+// DELETE a REVIEW
+export const deleteGameThunk = (data) => async (dispatch) => {
+	const response = await fetch(`/api/games/${data.id}`, {
+		method: "DELETE",
+	});
+	if (response.ok) {
+		dispatch(deleteGame(data.id));
+	}
+};
+
+const gameReducer = (state = {}, action) => {
+	let newState = { ...state };
+	switch (action.type) {
+		case LOAD_GAMES:
+			return { ...newState, ...action.payload };
+		case DELETE_GAMES:
+			delete newState[action.payload];
+			return newState;
+		default:
+			return state;
+    }
 };
 
 export default gameReducer;
